3f30c5f10c4f405ed15fc38d0154550ec34527ed
Prateek Jain <jain.prateek904@gmail.com>
2020-06-09 00:43:06 +0200

    show image code

---
+++ src/main/java/prtk/springframework/spring5recipeapp/services/ImageService.java
@@ -0,0 +1,7 @@
+package prtk.springframework.spring5recipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
